d776b30ba8b85e036d5626487c3f84340d63430d
shreyash1305D <shreyashwadi@gmail.com>
2025-12-10 21:37:54 +0530

    Add files via upload

---
+++ JpaRevesioController.java
@@ -0,0 +1,135 @@
+package com.example.jparevesion.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.jparevesion.entities.Account;
+import com.example.jparevesion.repositories.AccountRepository;
+
+@Controller
+public class JpaRevesioController {
+	@Autowired
+	private AccountRepository accRepo;
+	
+	
+	@GetMapping("/")
+	public String homePage()
+	{
+		System.out.println(accRepo.getAccountName(1052));
+		System.out.println(accRepo.existsByAccnm("shrutesh"));
+		List<Account> list=accRepo.findByBalanceBetween(20000, 40000);
+		list.stream().forEach(obj->System.out.println(obj.getAccnm()));
+		return "index.html";
+	}
+	
+	@GetMapping("/newacc")
+	public String newAccount()
+	{
+		return "newaccount.html";
+	}
+	
+	@PostMapping("/addacc")
+	public String addAccount(Account obj,Model m)
+	{
+		String msg="";
+		try
+		{
+			if(accRepo.existsById(obj.getAccno()))
+			{
+				throw new IllegalArgumentException("Account already exists");
+			}
+			
+			accRepo.save(obj);
+			msg="Account opened successfully";
+		}
+		catch(IllegalArgumentException e)
+		{
+			msg=e.getMessage();
+		}
+		
+		m.addAttribute("stat", msg);
+		return "accountcreation.jsp";
+	}
+	
+	@GetMapping("/allacc")
+	public String showAccounts(Model m)
+	{
+		List<Account> list=accRepo.findAll();
+		//list.stream().forEach(obj->System.out.println(obj.getAccnm()));
+		m.addAttribute("acclist", list);
+		return "accreport.jsp";
+	}
+	
+	@GetMapping("/depo")
+	public String depo()
+	{
+		return "depositform.html";
+	}
+	
+	@PostMapping("/deposit")
+	public String deposit(int accno,float amount)
+	{
+		System.out.println(accno);
+		System.out.println(amount);
+		
+		Account obj=accRepo.findById(accno).get();
+		System.out.println("Name : "+obj.getAccnm());
+		System.out.println("Type : "+obj.getAcctype());
+		System.out.println("Balance : "+obj.getBalance());
+		
+		obj.setBalance(obj.getBalance()+amount);
+		System.out.println("Balance : "+obj.getBalance());
+		accRepo.save(obj);
+		
+		return "depositdone.jsp";
+	}
+	
+	@GetMapping("/del")
+	public String del()
+	{
+		return "delete.html";
+	}
+	
+	@PostMapping("/delete")
+	public String delete(int accno,Model m)
+	{
+		String msg="";
+		
+		try
+		{
+		Account obj=accRepo.findById(accno).get();
+		accRepo.delete(obj);
+		msg="Account closed successfully";
+		}
+		catch(Exception e)
+		{
+			msg="Account not found";
+		}
+		
+		
+		m.addAttribute("stat", msg);
+		return "deleted.jsp";
+	}
+	
+	@GetMapping("/searchtype")
+	public String searchType()
+	{
+		return "searchtype.html";
+	}
+	
+	@PostMapping("/searchbytype")
+	public String searchByType(String acctype,Model m)
+	{
+		List<Account> list=accRepo.findByAcctype(acctype);
+		list.stream().forEach(obj->System.out.println(obj.getAccnm()));
+		
+		m.addAttribute("typ", acctype);
+		m.addAttribute("acclist", list);
+		return "listbytype.jsp";
+	}
+}
---
+++ JparevesionApplication.java
@@ -0,0 +1,14 @@
+package com.example.jparevesion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JparevesionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JparevesionApplication.class, args);
+		System.out.println("jpa running...");
+	}
+
+}
